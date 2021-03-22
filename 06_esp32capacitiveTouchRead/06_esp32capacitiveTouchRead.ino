5f74862ed950785f1bef029b89a5727a5fd66b07
Seth T <seth10@users.noreply.github.com>
2021-03-21 20:00:00 -0700

    Ignore spurious readings
    Just like with sketch_01, wait for 5 consecutive touch readings
    before printing that it is indeed touched. I also upped the polling
    rate from 5hz to 10hz so it's more responsive, even a half-second
    touch will trigger it.

--- 06_esp32capacitiveTouchRead/06_esp32capacitiveTouchRead.ino
+++ 06_esp32capacitiveTouchRead/06_esp32capacitiveTouchRead.ino
@@ -2,6 +2,7 @@
 // Just test touch pin - Touch0 is T0 which is on GPIO 4.
 
 int touchVal = 9999;
+int touchCount = 0;
 
 void setup()
 {
@@ -15,8 +16,16 @@ void loop()
   touchVal = touchRead(T0);
   Serial.print(touchVal);
   if (touchVal < 26) {
+    touchCount++;
+  } else {
+    touchCount = 0;
+  }
+  Serial.print(" - ");
+  Serial.print(touchCount);
+  
+  if (touchCount >= 5) {
     Serial.print(" - touched!");
   }
   Serial.println();
-  delay(200);
+  delay(100);
 }
