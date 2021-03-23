e118bc8cd2ae21633c84418cda254c6fa6dba6ce
Seth T <seth10@users.noreply.github.com>
2021-03-23 01:15:46 -0700

    Set the CA cert for WiFiClientSecure
    I was failing to connect because the certificate authority's certificate was not set. I had to go to spotify.com in Chrome, click the padlock in the top-left, click Certificate (Valid), click on the Certification Path tab, click on the root certificate (DigiCert), click View Certificate, click the Details tab, then click Copy To File.... Then click Next, switch the radio button to the second option (Base-64 encoded X. 509 (.CER) and click Next, pick a path and hit Next, then Finish and OK, OK, OK. Run the command `openssl x509 -in /path.cer -out /path.pem -outform PEM`. Open the .pem file in VS Code, use ctrl+alt+down to select the first column of every line, type ", then hit End, then type `\n" \`. Change the last `\ ` to a ; and copy the whole lot to a const char* digicert_ca.
    
    References:
    https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino
    https://techtutorialsx.com/2017/11/18/esp32-arduino-https-get-request/
    https://superuser.com/questions/1291416/download-pem-via-browser-only/1292098

--- 05_spotify/SpotifyClient.cpp
+++ 05_spotify/SpotifyClient.cpp
@@ -27,6 +27,29 @@
 
 #define min(X, Y) (((X)<(Y))?(X):(Y))
 
+const char* digicert_ca = \
+   "-----BEGIN CERTIFICATE-----\n" \
+  "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+  "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+  "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+  "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+  "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+  "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+  "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+  "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+  "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+  "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+  "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+  "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+  "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+  "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+  "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+  "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+  "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+  "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+  "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+  "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+  "-----END CERTIFICATE-----\n";
 
 
 SpotifyClient::SpotifyClient(String clientId, String clientSecret, String redirectUri) {
@@ -43,6 +66,7 @@ uint16_t SpotifyClient::update(SpotifyData *data, SpotifyAuth *auth) {
   isDataCall = true;
   currentParent = "";
   WiFiClientSecure client = WiFiClientSecure();
+  client.setCACert(digicert_ca);
   JsonStreamingParser parser;
   parser.setListener(this);
 
@@ -125,6 +149,7 @@ uint16_t SpotifyClient::playerCommand(SpotifyAuth *auth, String method, String c
   isDataCall = true;
   currentParent = "";
   WiFiClientSecure client = WiFiClientSecure();
+  client.setCACert(digicert_ca);
   JsonStreamingParser parser;
   parser.setListener(this);
 
@@ -199,6 +224,7 @@ void SpotifyClient::getToken(SpotifyAuth *auth, String grantType, String code) {
   JsonStreamingParser parser;
   parser.setListener(this);
   WiFiClientSecure client;
+  client.setCACert(digicert_ca);
   //https://accounts.spotify.com/api/token
   const char* host = "accounts.spotify.com";
   const int port = 443;
