3debcd63e21faf6690558b4707aa271fe4ca7f66
Seth T <seth10@users.noreply.github.com>
2021-03-23 00:21:00 -0700

    Change url from IP to local name
    Spotify didn't like me putting in an IP as my redirect URI.

--- 05_spotify/settings.h
+++ 05_spotify/settings.h
@@ -6,6 +6,6 @@ const char* password = "[redacted]";
 String clientId = "[redacted]";
 String clientSecret = "[redacted]";
 // These next two lines have to match. A client clientSpotifierHostName makes the
-// node listen on http://esp8266.local. Spotify needs an exact match of the redirect url
-String espotifierNodeName = "192.168.0.44";
-String redirectUri = "http%3A%2F%2F" + espotifierNodeName + "%2Fcallback%2F";
+// node listen on http://esp32.local. Spotify needs an exact match of the redirect url
+String espotifierNodeName = "esp32";
+String redirectUri = "http%3A%2F%2F" + espotifierNodeName + ".local%2Fcallback%2F";
